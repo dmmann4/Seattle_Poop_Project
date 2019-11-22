08f6391b66409d73448504e7afb80598089c3a79
Tellis <teellis20@gmail.com>
2019-11-21 18:41:52 -0800

    added directories for organization

---
+++ shared/logic.js
@@ -0,0 +1,104 @@
+airport way: 47.549801, -122.317596
+Dr. Jose: 47.592203, -122.317310
+Kobe park: 47.600898, -122.324987
+Madrona park: 47.611291, -122.285483
+Center for Wooden Boats:
+Lawton Park:
+Seward Park: 
+Greg Davis:
+N 143rd St & Linden Ave N:
+N 145th St & Linden Ave N:
+Piper's Creek Beach Area:
+Lake Union:
+Alaskan Way & Pike St:
+800 Alaskan Way S:
+Delridge Community Center:
+Mount Baker Park:
+Camp Long:
+Lincoln Park:
+Longfellow Creek- Dragon Fly:
+1950 Alaskan Way:
+Longfellow Creek - Greg Davis:
+Longfellow Creek - Thistle Street:
+Linden Square Apartments:
+Wharfside Apartments:
+Spring Lake Apartments:
+32nd Ave & E Cherry St:
+Maple Leaf Apartments:
+Fred Hutchinson Playground:
+1329 Sturgus Ave S:
+24th Ave S & S Dawson St:
+2470 Westlake Ave N:
+3907 SW 100th St:
+2601 S Ferdinand St:
+Lincoln Park:
+Fountain Court Apts:
+Skyline House:
+Bernard Apts:
+9000 36th Ave SW:
+10201 39th Ave Sw:
+2nd Ave & Denny Way:
+3710 SW Cloverdale St:
+Blue Dog Pond:
+Genesee: 
+Golden Gardens: 47.690359, -122.402513
+I-5 Colonnade Park:
+Magnuson Park:
+Northacres Park:
+Plymouth Pillars Park:
+Regrade Park:
+Westcrest Park:
+Woodland Park:
+Lower Kinnear:
+Magnolia Manor:
+2542 Westlake Ave N:
+919 N 70th St:
+Maggie Apartments:
+2764 Westlake Ave N:
+2nd Ave & Lenora St:
+3rd Ave & Lenora St - NW:
+2nd Ave & Bell St - E:
+Vine St & Western Ave:
+2nd Ave & Bell St - NW:
+2nd Ave & Wall St -SW:
+2nd Ave & Clay St -SW:
+Elliott Ave & Wall St - SE:
+8783 15th Ave NW:
+7003 17th Ave NW:
+NW 93rd St & 31st Ave NW:
+Audrey Apartments:
+Joule Apartments:
+Cairn Apartments:
+Taylor Apartments:
+Annaliese Apartments:
+Expo Apartments:
+Blue North:
+17th Ave S & Dallas Ave S:
+17th Ave S & S Donovan St:
+5711 24th Ave NW:
+NE 125th St & 16th Ave NE:
+Tilden School:
+Fuzzy Buddy's Dog Daycare:
+Dakota Place Park:
+Hirabayashi Place School:
+Astro Apts:
+Lake City: 
+2040 Westlake Ave N:
+NE 127th St & 33rd Ave NE:
+Union Bay Apts:
+Arbor Heights Element. School:
+Plymouth Apts:
+Kingfisher Natural Area:
+Victory Creek Park:
+Little Brook Park:
+Meadowbrook Park:
+
+
+
+
+
+
+
+
+// var longitude = ["47.652451", "47.651375", "47.655892", "47.654280", "47.670336", "47.679633", "47.694706", "47.696519", "47.690100", "47.712887", "47.708072", "47.708618", "47.682788", "47.679560", ""];
+// var latitude = ["-122.392238", "-122.392885", "-122.391889", "-122.374626", "-122.387729", "-122.379450", "-122.377250", "-122.396926", "-122.402461", "-122.378633", "-122.344843", "-122.315850", "-122.323026", "-122.346362", ""];
